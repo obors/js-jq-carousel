8aac138df52fcda156e95148a247828a401e5cad
Oleg <obors@ymail.com>
2020-05-04 23:03:29 +0200

    index3

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+
+// $(document).ready(function (){
+//
+//   $(".btn-next").click(function(){
+//
+//   var currImg=$(".active");
+//   var nextImg=currImg.next();
+//
+//   if (nextImg.length){
+//     currImg.removeClass("active").css("z-index", -10);
+//     nextImg.addClass("active").css("z-index", 10);
+//   }
+//
+// });
+//
+// $(".btn-prev").click(function(){
+//   var currImg=$(".active");
+//   var prevImg=currImg.next();
+//
+//   if (nextImg.length){
+//     currImg.removeClass("active").css("z-index", -10);
+//     prevImg.addClass("active").css("z-index", 10);
+//   }
+//
+//
+// })
---
+++ main1.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function () {
+//
+//   var  slides = ("slides");
+//   $(".prev").on("click", function () {
+//
+//     slider.animate({ left: "0%" }, 300,
+//       function () {
+//
+//         slider.css("left", "-100%");
+//           $(".img").first().before($(".slide").last())
+//   });
+//
+//
+// });
+//   $(".prev").on("click", function () {
+//
+//     slider.animate({left: "-200%" }, 300,
+//       function () {
+//
+//         slider.css("left", "-100%");
+//           $(".img").last().before($(".slide").first())
+//   });
+//
+//
+//   });
+//
+//   })
